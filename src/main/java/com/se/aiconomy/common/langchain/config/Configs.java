9be2d0a87bca840f7d62546c76d0b99627304451
Charles <1447393600@qq.com>
2025-03-25 15:52:58 +0800

    :bug: Fix bug when .env is missing

--- src/main/java/com/se/aiconomy/common/langchain/config/Configs.java
+++ src/main/java/com/se/aiconomy/common/langchain/config/Configs.java
@@ -7,7 +7,7 @@ import static dev.langchain4j.internal.Utils.getOrDefault;
 import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
 
 public class Configs {
-    public static final Dotenv dotenv = Dotenv.load();
+    public static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
     public static final String BASE_URL = getOrDefault(System.getenv("BASE_URL"), dotenv.get("BASE_URL"));
     public static final String API_KEY = getOrDefault(System.getenv("API_KEY"), dotenv.get("API_KEY"));
     public static final OpenAiChatModelName MODEL = GPT_4_O_MINI;
