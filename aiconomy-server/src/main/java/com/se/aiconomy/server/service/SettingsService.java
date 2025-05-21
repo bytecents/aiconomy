977b16ee4ee7938424fb029b2dbbb8dc8b12c911
ysl <m2843876602@outlook.com>
2025-05-21 23:18:58 +0800

    ✨ feat(server): New user information update processing and obtaining user setting list functions Author: Shiyu Li <jp2022213661@qmul.ac.uk>

--- aiconomy-server/src/main/java/com/se/aiconomy/server/service/SettingsService.java
+++ aiconomy-server/src/main/java/com/se/aiconomy/server/service/SettingsService.java
@@ -2,6 +2,10 @@ package com.se.aiconomy.server.service;
 
 import com.se.aiconomy.server.model.entity.Settings;
 
+import java.util.List;
+
 public interface SettingsService {
     void updateSettings(Settings settings);
+
+    List<Settings> getSettingsByUserId(String userId);
 }
