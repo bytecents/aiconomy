e538d26c45381d1b7f07d676dcda68aa217d89bb
elfishecho <1157650439@qq.com>
2025-05-23 21:34:40 +0800

    ✨ feat(server):
    Author: Youhao Zhong <jp2022213676@qmul.ac.uk>

--- aiconomy-server/src/main/java/com/se/aiconomy/server/model/dto/account/request/UpdateAccountRequest.java
+++ aiconomy-server/src/main/java/com/se/aiconomy/server/model/dto/account/request/UpdateAccountRequest.java
@@ -1,24 +1,14 @@
 package com.se.aiconomy.server.model.dto.account.request;
 
 import com.se.aiconomy.server.model.entity.Account;
+import lombok.*;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UpdateAccountRequest {
     private String userId;
     private Account account;
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public Account getAccount() {
-        return account;
-    }
-
-    public void setAccount(Account account) {
-        this.account = account;
-    }
 }
