54c9f34100b738bb56e621715ef4b82a91035fdc
Zheyun Zhao <2022213670@bupt.cn>
2025-05-25 05:11:18 +0800

    :wastebasket: chore(project): clearup langchain codes
    Author: Zheyun Zhao <jp2022213670@qmul.ac.uk>

--- aiconomy-langchain/src/main/java/com/se/aiconomy/langchain/service/analysis/budget/Assistant.java
+++
@@ -1,7 +0,0 @@
-package com.se.aiconomy.langchain.service.analysis.budget;
-
-import com.se.aiconomy.langchain.common.model.Budget.AIAnalysis;
-
-public interface Assistant {
-    AIAnalysis analyzeBudgetFrom(String text);
-}
--- aiconomy-langchain/src/main/java/com/se/aiconomy/langchain/service/classification/Assistant.java
+++
@@ -1,7 +0,0 @@
-package com.se.aiconomy.langchain.service.classification;
-
-import com.se.aiconomy.langchain.common.model.BillType;
-
-public interface Assistant {
-    BillType classifyTransactionFrom(String text);
-}
